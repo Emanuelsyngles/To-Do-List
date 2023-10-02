a1232f1f4628396e1212fc91cdb5ae21673f1ca6
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-02 18:07:06 -0300

    Bug corrigido

--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -1,20 +1,22 @@
 import {useState} from 'react'
 
-const [value, setValue] = useState("")
-const [category, setCategory] = useState("")
+const TodoForm = () => {
+  const[value, setValue] = useState()
+  const[category, setCategory] = useState()
+
 
 const handleSubmit = (e) => {
   e.preventDefault()
+  if(!value || !category) return ;;
   console.log(value, category)
 }
 
-const TodoForm = () => {
   return (
     <div className="todo-form">
       <h2>Criar tarefa</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Digite o titulo" onChange={(e) => setValue (e.target.value)}/>
-        <select onChange={(e) => setCategory (e.target.value)}>
+        <input type="text" placeholder="Digite o titulo" onChange={(e) => setValue(e.target.value)}/>
+        <select onChange={(e) => setCategory(e.target.value)}>
           <option value="">Selecione uma categoria</option>
           <option value="trabalho">Trabalho</option>
           <option value="Pessoal">Pessoal</option>
@@ -25,5 +27,7 @@ const TodoForm = () => {
     </div>
   )
 }
+
+
 
 export default TodoForm
