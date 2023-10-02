acc6536868a47133ab83330ea538a2fbb1efad8d
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-01 22:02:15 -0300

    Gerênciando funções

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -30,8 +30,13 @@ function App() {
         <div className="todo">
           <div className="content">
             <p>{todo.text}</p>
+            <p className="category">({todo.category})</p>
+    </div>
+          <div>
+            <button>Completar</button>
+            <button>X</button>
           </div>
-        </div>
+    </div>
       ))}
     </div>
   </div>
