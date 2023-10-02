4ca0dd8923312ba882f4bc2ac72428457ebe6748
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-02 13:38:18 -0300

    Adicionando opções

--- todo/src/components/TodoForm.jsx
+++ todo/src/components/TodoForm.jsx
@@ -6,7 +6,12 @@ const TodoForm = () => {
       <h2>Criar tarefa</h2>
       <form>
       <input type="text" placeholder="Digite o titulo"/>
-      <select></select>
+      <select>
+        <option value="">Selecione uma categoria</option>
+        <option value="Trabalho">Trabalho</option>
+        <option value="Pessoal">Pessoal</option>
+        <option value="Estudos">Estudos</option>
+      </select>
       </form>
     </div>
   )
