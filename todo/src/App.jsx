29f38b78aa8a2be45b196d211f7e79dcf40d8bc7
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-01 21:52:49 -0300

    useState

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,7 +2,26 @@ import { useState } from 'react'
 
 
 function App() {
-  const [todos, setTodos] = useState()
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "Estudar react",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ])
 
   return (
     <div>Olá react!</div>
