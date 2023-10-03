0ee73b336582148169bf353263b8b464a26ea5e4
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-03 15:29:37 -0300

    Search

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Todo = ({ todo, removeTodo, completeTodo }) => {
 
   return (
-  <div className="todo" style={{textDecoration: todo.isCompleted ? "line-trough" : ""}}>
+  <div className="todo" style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
     <div className="content">
       <p>{todo.text}</p>
       <p className="category">
