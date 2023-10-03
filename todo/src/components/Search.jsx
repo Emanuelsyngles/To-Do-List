82a6b848f35f316768dd84d0ba141c74e6a426ed
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-03 15:49:02 -0300

    Update Search.jsx

--- todo/src/components/Search.jsx
+++ todo/src/components/Search.jsx
@@ -2,7 +2,7 @@ const Search = ({Search, setSearch}) => {
   return (
     <div className="search">
         <h2>Pesquisar:</h2>
-        <input type="text" value={Search}/>
+        <input type="text" value={Search} onChange={(e)=> setSearch(e.target.value)}/>
     </div>
   )
 }
