bae0a3546126408eed13b8a0c6280118cb136c72
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2023-10-03 15:33:34 -0300

    Configurando o search

---
+++ todo/src/components/Search.jsx
@@ -0,0 +1,10 @@
+const Search = ({Search, setSearch}) => {
+  return (
+    <div className="search">
+        <h2>Pesquisar:</h2>
+        <input type="text" value={Search}/>
+    </div>
+  )
+}
+
+export default Search
